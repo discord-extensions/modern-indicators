ef25c2311bc8844c603c94a3272570a3d8b67c15
Colt <39076891+LuckFire@users.noreply.github.com>
2023-09-20 17:28:10 -0400

    fix: padding offset

--- theme-scss.config.js
+++ theme-scss.config.js
@@ -2,7 +2,7 @@
 const baseAuthor = {
     name: 'Modern Indicators',
     author: 'LuckFire',
-    version: '2.2.0',
+    version: '2.2.1',
     description: 'Updates Discord\'s indicators to feel more modern.'
 }
 
